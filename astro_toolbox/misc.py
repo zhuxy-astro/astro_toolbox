ae337e2aeef76a85ff273f494ec7610f870d8703
ZhuXY <tonyzhu77@126.com>
2025-03-30 21:59:07 +0800

    Update: misc.Bar can automatically run in Jupyter notebooks

--- astro_toolbox/misc.py
+++ astro_toolbox/misc.py
@@ -1,38 +1,94 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+I try to unify the usage of progress bar from alive_bar and astropy, and also my
+own progress bar.
+Using the astropy progress bar will partly mess up the output in multiprocessing.
+Using my own progress bar may be slow for large loops, and may not work in
+Jupyter notebook.
 
-# %% import
-import sys
-from time import time
+Both of the two need additional parameters to work properly in Jupyter notebook:
+    - alive_bar: force_tty=True
+    - astropy: ipython_widget=True
+I have redefined the two progress bars as Bar in this file, which should
+automatically detect the environment. It should work fine without additional
+parameters.
 
+Usage:
 
-# %% progress bar
-# unify the usage of progress bar
-# using the astropy progress bar will partly mess up the output in multiprocessing
-"""Usage:
 from misc import Bar
 LENGTH = 100
 with Bar(LENGTH) as bar:
     for i in range(LENGTH):
         bar()
-
-In Jupyter notebook, add `force_tty=True` if using alive_bar
-or `ipython_widget=True` if using astropy progress bar in the definition of Bar.
 """
-from astropy.utils.console import ProgressBar  # class
 
+
+# %% import
+import sys
+from time import time
 
 try:
-    from alive_progress import alive_bar as Bar  # func
+    from astropy.utils.console import ProgressBar as _ProgressBar  # class
+    has_astropy = True
 except ImportError:
-    class Bar(ProgressBar):
+    has_astropy = False
+try:
+    from alive_progress import config_handler, alive_bar  # func
+    config_handler.set_global(force_tty=True)
+    has_alive_bar = True
+except ImportError:
+    has_alive_bar = False
+
+
+# %%
+def in_ipython():
+    """Return value:
+    0: Standard Python interpreter
+    1: IPython terminal
+    2: Jupyter notebook or qtconsole
+    3: Other type (maybe unknown)
+    """
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            # Jupyter notebook or qtconsole
+            return 2
+        elif shell == "TerminalInteractiveShell":
+            # Terminal running IPython
+            return 1
+        else:
+            # Other type (maybe unknown)
+            return -1
+    except NameError:
+        # get_ipython not defined, so likely standard Python interpreter
+        return 0
+
+
+is_in_ipython = in_ipython()
+
+
+# %% redefine ProgressBar
+if has_astropy:
+    class ProgressBar(_ProgressBar):
+        """ProgressBar aware of IPython, and accepting force_tty"""
+        def __init__(self, *args, **kwargs):
+            force_tty = kwargs.pop('force_tty', False)
+            if 'ipython_widget' not in kwargs:
+                if force_tty:
+                    # when force_tty is set, follow it
+                    kwargs['ipython_widget'] = True
+                else:
+                    # when nothing is set, automatically detect the environment
+                    kwargs['ipython_widget'] = is_in_ipython == 2
+            super().__init__(*args, **kwargs)
+
         def __call__(self):
             return self.update()
 
 
 # %% MyBar
 class MyBar:
-    # 自己做的进度条。print的过程对超大循环来说可能有点费时间。现在可用alive_bar替换。
     def __init__(self, scale):
         self.scale = scale
         self.prog = 0
@@ -55,3 +111,12 @@ class MyBar:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print()
+
+
+# %% Choose a progress bar
+if has_alive_bar:
+    Bar = alive_bar
+elif has_astropy:
+    Bar = ProgressBar
+else:
+    Bar = MyBar
